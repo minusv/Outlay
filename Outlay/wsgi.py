0ec9038b0ef75bd1dc6ebb428863a7208e2f478e
minusv <msmanasup@gmail.com>
2018-10-14 13:12:34 +0530

    Project name changed

--- Outlay/wsgi.py
+++ Outlay/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for WWStay project.
+WSGI config for Outlay project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WWStay.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Outlay.settings")
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WWStay.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Outlay.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
